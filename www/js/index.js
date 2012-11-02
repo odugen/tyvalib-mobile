f7277eb6d69dcafcaa8efc4771ec0eee03b42cb1
Alexey Oyun <alexey.oyun@gmail.com>
2012-11-02 22:51:52 +0400

    remove jquery mobile my.js and some markup

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,21 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 var app = {
     // Application Constructor
     initialize: function () {
@@ -36,6 +36,7 @@ var app = {
     onDeviceReady: function () {
         $('form').submit(function (e) {
             e.preventDefault();
+            console.log('Submit search');
             app.search($('#q').val());
             return false;
         });
@@ -46,10 +47,11 @@ var app = {
         console.log('show: ' + word);
         return word;
     },
+    counter: 0,
     search: function (term) {
-        console.log('load recent');
+        console.log('Search ' + term + ' ' + (++app.counter));
         $.ajax({
-//            url: 'http://localhost:59468/api/word',
+            //            url: 'http://localhost:59468/api/word',
             url: 'http://tyvalib.ru/api/word',
             data: { term: term || '' },
             type: "GET",
@@ -69,11 +71,8 @@ var app = {
                     $('#results').html('Not found!');
                 }
             },
-            complete: function () {
-                console.log('complete');
-            },
             error: function (request, error) {
-                console.log('Request: ' + JSON.stringify(request) + ' Error: ' + JSON.stringify(error));
+                alert('Request: ' + JSON.stringify(request) + ' Error: ' + JSON.stringify(error));
             }
         });
     }
